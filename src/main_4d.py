fixed gradient backprop of gate variables

src/main_4d.py
@@ -7,6 +7,7 @@ import utils.load_data as dh
 from sklearn.model_selection import train_test_split
 import time
 import torch
+import pickle
 
 if __name__ == '__main__':
 
@@ -19,62 +20,74 @@ if __name__ == '__main__':
     FEATURES_FULL = ['FSC-A', 'FSC-H', 'SSC-H', 'CD45', 'SSC-A', 'CD5', 'CD19', 'CD10', 'CD79b', 'CD3']
     FEATURE2ID = dict((FEATURES[i], i) for i in range(len(FEATURES)))
     LOGISTIC_K = 10.0
-    REGULARIZATION_PENALTY = 1.
+    REGULARIZATION_PENALTY = 0.
+    LOAD_DATA_FROM_PICKLE = True
 
     # x: a list of samples, each entry is a numpy array of shape n_cells * n_features
     # y: a list of labels; 1 is CLL, 0 is healthy
-    x, y = dh.load_cll_data(DIAGONOSIS_FILENAME, CYTOMETRY_DIR, FEATURES_FULL)
-    x = dh.filter_cll_4d(x)
+    if LOAD_DATA_FROM_PICKLE:
+        with open(DATA_DIR + "filtered_4d_x_list.pkl", 'rb') as f:
+            x = pickle.load(f)
+        with open(DATA_DIR + 'y_list.pkl', 'rb') as f:
+            y = pickle.load(f)
+    else:
+        x, y = dh.load_cll_data(DIAGONOSIS_FILENAME, CYTOMETRY_DIR, FEATURES_FULL)
+        x = dh.filter_cll_4d(x)
+        with open(DATA_DIR + 'filtered_4d_x_list.pkl', 'wb') as f:
+            pickle.dump(x, f)
+        with open(DATA_DIR + 'y_list.pkl', 'wb') as f:
+            pickle.dump(y, f)
     # scale the data
-    x = [_/1000.0 for _ in x]
+    x = [_ / 1000.0 for _ in x]
     print("Number of cells in each sample after filtering:", [_.shape[0] for _ in x])
-    x_train, x_eval, y_train, y_eval = train_test_split(x, y, test_size=0.10, random_state=123)
+    x_train, x_eval, y_train, y_eval = train_test_split(x, y, test_size=0.30, random_state=123)
     x_train = [torch.tensor(_, dtype=torch.float32) for _ in x_train]
     x_eval = [torch.tensor(_, dtype=torch.float32) for _ in x_eval]
     y_train = torch.tensor(y_train, dtype=torch.float32)
     y_eval = torch.tensor(y_eval, dtype=torch.float32)
 
-
     print("Running time for loading the data: %.3f seconds." % (time.time() - start))
 
     nested_list = \
         [
-            [[u'CD5', 1638./1000, 3891./1000], [u'CD19', 2150./1000, 3891./1000]],
+            [[u'CD5', 1638. / 1000, 3891. / 1000], [u'CD19', 2150. / 1000, 3891. / 1000]],
             [
                 [
-                    [[u'CD10', 0, 1228./1000], [u'CD79b', 0, 1843./1000]],
+                    [[u'CD10', 0, 1228. / 1000], [u'CD79b', 0, 1843. / 1000]],
                     []
                 ]
             ]
         ]
-    # nested_list = \
-    #     [
-    #         [[u'CD5', 1000./1000, 3891./1000], [u'CD19', 1000./1000, 3891./1000]],
-    #         [
-    #             [
-    #                 [[u'CD10', 1000./1000, 1228./1000], [u'CD79b', 1000./1000, 1843./1000]],
-    #                 []
-    #             ]
-    #         ]
-    #     ]
+    nested_list_init = \
+        [
+            [[u'CD5', 2000. / 1000, 3000 / 1000], [u'CD19', 2000. / 1000, 3000. / 1000]],
+            [
+                [
+                    [[u'CD10', 1000. / 1000, 2000. / 1000], [u'CD79b', 1000. / 1000, 2000. / 1000]],
+                    []
+                ]
+            ]
+        ]
     reference_tree = ReferenceTree(nested_list, FEATURE2ID)
+    init_tree = ReferenceTree(nested_list_init, FEATURE2ID)
+    # init_tree = None
 
     # Just for sanity check...
-    # for logistic_k in [1, 10, 100, 1000, 10000]:
-    #     model_tree = ModelTree(reference_tree, logistic_k=logistic_k, regularisation_penalty=REGULARIZATION_PENALTY)
-    #     # extract features with bounding boxes in the reference tree;
-    #     threshold = 0.0252
-    #     features_train = model_tree(x_train, y_train)['leaf_probs'].numpy()[:, 0]
-    #     features_eval = model_tree(x_eval, y_eval)['leaf_probs'].numpy()[:, 0]
-    #     y_pred_train = (features_train > threshold) * 1.0
-    #     y_pred_eval = (features_eval > threshold) * 1.0
-    #     print("With SOFT features(steepness = %d) extracted with bounding boxes in reference tree..." % logistic_k)
-    #     print("Acc on trainning and eval data: %.3f, %.3f"% (
-    #         sum((y_pred_train == y_train.numpy())) * 1.0 / len(x_train),
-    #         sum((y_pred_eval == y_eval.numpy())) * 1.0 / len(x_eval)))
+    for logistic_k in [1, 10, 100, 1000, 10000]:
+        model_tree = ModelTree(reference_tree, logistic_k=logistic_k, regularisation_penalty=REGULARIZATION_PENALTY)
+        # extract features with bounding boxes in the reference tree;
+        threshold = 0.0252
+        features_train = model_tree(x_train, y_train)['leaf_probs'].detach().numpy()[:, 0]
+        features_eval = model_tree(x_eval, y_eval)['leaf_probs'].detach().numpy()[:, 0]
+        y_pred_train = (features_train > threshold) * 1.0
+        y_pred_eval = (features_eval > threshold) * 1.0
+        print("With SOFT features(steepness = %d) extracted with bounding boxes in reference tree..." % logistic_k)
+        print("Acc on training and eval data: %.3f, %.3f" % (
+            sum((y_pred_train == y_train.numpy())) * 1.0 / len(x_train),
+            sum((y_pred_eval == y_eval.numpy())) * 1.0 / len(x_eval)))
 
-    model_tree = ModelTree(reference_tree, logistic_k=LOGISTIC_K, regularisation_penalty=REGULARIZATION_PENALTY)
-
+    model_tree = ModelTree(reference_tree, logistic_k=LOGISTIC_K, regularisation_penalty=REGULARIZATION_PENALTY,
+                           init_tree=init_tree)
 
     # Keep track of losses for plotting
     train_loss = []
@@ -88,10 +101,10 @@ if __name__ == '__main__':
 
     n_epoch = 1000
     batch_size = len(x_train)
-    n_epoch_print = 50
+    n_epoch_print = 20
 
     # optimizer
-    learning_rate = 3.14
+    learning_rate = 1.0
     # optimizer = torch.optim.Adam(model_tree.parameters(), lr=learning_rate)
     optimizer = torch.optim.SGD(model_tree.parameters(), lr=learning_rate)
 
@@ -108,7 +121,6 @@ if __name__ == '__main__':
         n_mini_batch = len(x_train) // batch_size
 
         for i in range(n_mini_batch):
-
             # generate mini batch data
             idx_batch = [_ for _ in range(batch_size * i, batch_size * (i + 1))]
             x_batch = [x_train[_] for _ in idx_batch]
@@ -134,6 +146,8 @@ if __name__ == '__main__':
 
         # print every n_batch_print mini-batches
         if epoch % n_epoch_print == n_epoch_print - 1:
+            print(output)
+            print(model_tree)
             train_loss_avg = sum(train_loss[-n_mini_batch:]) * 1.0 / n_mini_batch
             train_acc_avg = sum(train_acc[-n_mini_batch:]) * 1.0 / n_mini_batch
             # eval
@@ -146,11 +160,10 @@ if __name__ == '__main__':
             eval_precision.append(precision_score(y_eval.data.numpy(), y_pred, average='macro'))
             eval_recall.append(recall_score(y_eval.data.numpy(), y_pred, average='macro'))
             print(model_tree)
+            print(output_eval['reg_loss'], output_eval['loss'])
             # print(model_tree.linear.weight, model_tree.linear.bias, -model_tree.linear.bias/model_tree.linear.weight)
             print('[Epoch %d, batch %d] training, eval loss: %.3f, %.3f' % (epoch, i, train_loss_avg, eval_loss[-1]))
             print('[Epoch %d, batch %d] training, eval acc: %.3f, %.3f' % (epoch, i, train_acc_avg, eval_acc[-1]))
 
     print("Running time for training %d epoch: %.3f seconds" % (n_epoch, time.time() - start))
     print('Finished Training')
-
-

src/test.py
@@ -4,6 +4,7 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 import numpy as np
 from utils.bayes_gate_pytorch import *
+import time
 
 if __name__ == '__main__':
 
@@ -27,23 +28,36 @@ if __name__ == '__main__':
                 ]
             ]
         ]
+
+    nested_list_init = \
+        [
+            [[u'CD5', 0.5, 0.6], [u'CD19', 0.5, 0.6]],
+            [
+                [
+                    [[u'CD10', 0.4, 0.8], [u'CD79b', 0.4, 0.8]],
+                    []
+                ]
+            ]
+        ]
     FEATURES = ['CD5', 'CD19', 'CD10', 'CD79b']
     FEATURE2ID = dict((FEATURES[i], i) for i in range(len(FEATURES)))
     LOGISTIC_K = 10
     REGULARIZATION_PENALTY = 10.
     reference_tree = ReferenceTree(nested_list, FEATURE2ID)
+    init_tree = ReferenceTree(nested_list_init, FEATURE2ID)
 
     # done: Test ModelNode works: only the first gate [[u'CD5', 1638, 3891], [u'CD19', 2150, 3891]] is used.
-    model_node = ModelNode(LOGISTIC_K, reference_tree)
+    model_node = ModelNode(LOGISTIC_K, reference_tree, init_tree)
     for name, param in model_node.named_parameters():
         if param.requires_grad:
             print(name, param.data)
     # todo: check results
     logp, reg_penalty = model_node(x_train[0])
-    print(logp)
+    # print(logp)
 
     # done: test ModelTree
-    model_tree = ModelTree(reference_tree, logistic_k=LOGISTIC_K, regularisation_penalty=REGULARIZATION_PENALTY)
+    model_tree = ModelTree(reference_tree, logistic_k=LOGISTIC_K, regularisation_penalty=REGULARIZATION_PENALTY,
+                           init_tree=init_tree)
     print(model_tree)
     print(model_tree.linear.weight, model_tree.linear.bias)
     print(model_tree.children_dict.items())
@@ -52,7 +66,7 @@ if __name__ == '__main__':
     leaf_probs = output['leaf_probs']
     y_pred = output['y_pred']
     loss = output['loss']
-    print("leaf_probs:", leaf_probs)
+    # print("leaf_probs:", leaf_probs)
 
     # todo: train the model
     # Keep track of losses for plotting
@@ -65,28 +79,28 @@ if __name__ == '__main__':
     train_recall = []
     eval_recall = []
 
-    n_epoch = 2
-    batch_size = 10
-    n_batch_print = 10
+    n_epoch = 100
+    batch_size = int(len(x_train)/10)
+    n_epoch_print = 20
 
     # optimizer
-    learning_rate = 1.0
-    optimizer = torch.optim.Adam(model_tree.parameters(), lr=learning_rate)
-    # optimizer = optim.SGD(model_tree.parameters(), lr=0.001, momentum=0.9)
+    learning_rate = 3.14
+    # optimizer = torch.optim.Adam(model_tree.parameters(), lr=learning_rate)
+    optimizer = torch.optim.SGD(model_tree.parameters(), lr=learning_rate)
+
+    start = time.time()
 
     for epoch in range(n_epoch):
-
-        running_loss_train = 0.0
 
         # shuffle training data
         idx_shuffle = np.array([_ for _ in range(len(x_train))])
         shuffle(idx_shuffle)
-
         x_train = [x_train[_] for _ in idx_shuffle]
         y_train = y_train[idx_shuffle]
-        sample_id_train = [sample_id_train[_] for _ in idx_shuffle]
 
-        for i in range(len(x_train) // batch_size):
+        n_mini_batch = len(x_train) // batch_size
+
+        for i in range(n_mini_batch):
 
             # generate mini batch data
             idx_batch = [_ for _ in range(batch_size * i, batch_size * (i + 1))]
@@ -98,36 +112,38 @@ if __name__ == '__main__':
 
             # forward + backward + optimize
             output = model_tree(x_batch, y_batch)
-            leaf_probs = output['leaf_probs']
             loss = output['loss']
-            y_pred = (output['y_pred'].data.numpy() > 0) * 1.0
-            y_batch = y_batch.data.numpy()
             loss.backward()
             optimizer.step()
 
             # statistics
+            y_pred = (output['y_pred'].data.numpy() > 0.5) * 1.0
+            y_batch = y_batch.data.numpy()
+            # leaf_probs = output['leaf_probs']
             train_loss.append(output['loss'])
             train_acc.append(sum(y_pred == y_batch) * 1.0 / batch_size)
             train_precision.append(precision_score(y_batch, y_pred, average='macro'))
             train_recall.append(recall_score(y_batch, y_pred, average='macro'))
 
-            # print every n_batch_print mini-batches
-            if i % n_batch_print == n_batch_print - 1:
-                train_loss_avg = sum(train_loss[-n_batch_print:]) * 1.0 / n_batch_print
-                train_acc_avg = sum(train_acc[-n_batch_print:]) * 1.0 / n_batch_print
+        # print every n_batch_print mini-batches
+        if epoch % n_epoch_print == n_epoch_print - 1:
+            train_loss_avg = sum(train_loss[-n_mini_batch:]) * 1.0 / n_mini_batch
+            train_acc_avg = sum(train_acc[-n_mini_batch:]) * 1.0 / n_mini_batch
+            # eval
+            output_eval = model_tree(x_eval, y_eval)
+            # leaf_probs = output_eval['leaf_probs']
+            y_pred = (output_eval['y_pred'].data.numpy() > 0.5) * 1.0
+            eval_loss.append(output_eval['loss'])
+            eval_acc.append(sum(y_pred == y_eval.data.numpy()) * 1.0 / len(x_eval))
+            eval_precision.append(precision_score(y_eval.data.numpy(), y_pred, average='macro'))
+            eval_recall.append(recall_score(y_eval.data.numpy(), y_pred, average='macro'))
+            print(model_tree)
+            print("=================")
+            print(output_eval['reg_loss'], output_eval['loss'], nn.BCELoss(output_eval['y_pred'], y_eval))
+            # print(model_tree.linear.weight, model_tree.linear.bias, -model_tree.linear.bias/model_tree.linear.weight)
+            print('[Epoch %d, batch %d] training, eval loss: %.3f, %.3f' % (epoch, i, train_loss_avg, eval_loss[-1]))
+            print('[Epoch %d, batch %d] training, eval acc: %.3f, %.3f' % (epoch, i, train_acc_avg, eval_acc[-1]))
 
-                # eval
-                output_eval = model_tree(x_eval, y_eval)
-                # leaf_probs = output_eval['leaf_probs']
-                y_pred = (output_eval['y_pred'].data.numpy() > 0) * 1.0
-                eval_loss.append(output_eval['loss'])
-                eval_acc.append(sum(y_pred == y_eval.data.numpy()) * 1.0 / len(x_eval))
-                eval_precision.append(precision_score(y_eval.data.numpy(), y_pred, average='macro'))
-                eval_recall.append(recall_score(y_eval.data.numpy(), y_pred, average='macro'))
-
-                print('[%d, %5d] training, eval loss: %.3f, %.3f' % (epoch, i, train_loss_avg, eval_loss[-1]))
-                print('[%d, %5d] training, eval acc: %.3f, %.3f' % (epoch, i, train_acc_avg, eval_acc[-1]))
-
+    print("Running time for training %d epoch: %.3f seconds" % (n_epoch, time.time() - start))
     print('Finished Training')
 
-    # todo: plotting

src/utils/bayes_gate_pytorch.py
@@ -45,7 +45,7 @@ class ReferenceTree(object):
 
 
 class ModelNode(nn.Module):
-    def __init__(self, logistic_k, reference_tree):
+    def __init__(self, logistic_k, reference_tree, init_tree=None):
         """
         :param logistic_k:
         :param reference_tree:
@@ -56,10 +56,16 @@ class ModelNode(nn.Module):
         self.reference_tree = reference_tree
         self.gate_dim1 = self.reference_tree.gate.gate_dim1
         self.gate_dim2 = self.reference_tree.gate.gate_dim2
-        self.gate_low1 = nn.Parameter(self.reference_tree.gate.gate_low1)
-        self.gate_low2 = nn.Parameter(self.reference_tree.gate.gate_low2)
-        self.gate_upp1 = nn.Parameter(self.reference_tree.gate.gate_upp1)
-        self.gate_upp2 = nn.Parameter(self.reference_tree.gate.gate_upp2)
+        if init_tree == None:
+            self.gate_low1 = nn.Parameter(self.reference_tree.gate.gate_low1)
+            self.gate_low2 = nn.Parameter(self.reference_tree.gate.gate_low2)
+            self.gate_upp1 = nn.Parameter(self.reference_tree.gate.gate_upp1)
+            self.gate_upp2 = nn.Parameter(self.reference_tree.gate.gate_upp2)
+        else:
+            self.gate_low1 = nn.Parameter(init_tree.gate.gate_low1)
+            self.gate_low2 = nn.Parameter(init_tree.gate.gate_low2)
+            self.gate_upp1 = nn.Parameter(init_tree.gate.gate_upp1)
+            self.gate_upp2 = nn.Parameter(init_tree.gate.gate_upp2)
 
     def __repr__(self):
         repr_string = ('ModelNode(\n'
@@ -95,7 +101,7 @@ class ModelNode(nn.Module):
 
 class ModelTree(nn.Module):
 
-    def __init__(self, reference_tree, logistic_k=10, regularisation_penalty=10.):
+    def __init__(self, reference_tree, logistic_k=10, regularisation_penalty=10., init_tree=None):
         """
         :param args: pass values for variable n_cell_features, n_sample_features,
         :param kwargs: pass keyworded values for variable logistic_k=?, regularisation_penality=?.
@@ -104,24 +110,31 @@ class ModelTree(nn.Module):
         self.logistic_k = logistic_k
         self.regularisation_penalty = regularisation_penalty
         self.children_dict = nn.ModuleDict()
-        self.root = self.add(reference_tree)
+        self.root = self.add(reference_tree,init_tree)
         self.n_sample_features = reference_tree.n_leafs
 
         # define parameters in the logistic regression model
-        self.linear = torch.nn.Linear(self.n_sample_features, 1)
-        self.criterion = nn.BCELoss()
+        self.linear = nn.Linear(self.n_sample_features, 1)
+        self.criterion = nn.BCEWithLogitsLoss()
 
-    def add(self, reference_tree):
+    def add(self, reference_tree, init_tree=None):
         """
         construct self.children_dict dictionary for all nodes in the tree structure.
         :param reference_tree:
         :return:
         """
-        node = ModelNode(self.logistic_k, reference_tree)
+        node = ModelNode(self.logistic_k, reference_tree, init_tree)
         child_list = nn.ModuleList()
-        for child in reference_tree.children:
-            child_node = self.add(child)
-            child_list.append(child_node)
+        if init_tree == None:
+            for child in reference_tree.children:
+                child_node = self.add(child)
+                child_list.append(child_node)
+        else:
+            for _ in range(len(reference_tree.children)):
+                child_ref = reference_tree.children[_]
+                child_init = init_tree.children[_]
+                child_node = self.add(child_ref, child_init)
+                child_list.append(child_node)
         self.children_dict.update({str(id(node)): child_list})
         return node
 
@@ -140,33 +153,32 @@ class ModelTree(nn.Module):
                   'y_pred': None}
         loss = 0.0
 
-        for idx in range(len(x)):
-            leaf_probs = []
+        leaf_probs = y.new_zeros((y.shape[0], self.n_sample_features))
+        for sample_idx in range(len(x)):
 
-            thislevel = [(self.root, torch.zeros((x[idx].shape[0],)))]
-            while thislevel:
-                nextlevel = list()
-                for (node, pathlogp) in thislevel:
+            this_level = [(self.root, torch.zeros((x[sample_idx].shape[0],)))]
+            leaf_idx = 0
+            while this_level:
+                next_level = list()
+                for (node, pathlogp) in this_level:
+                    logp, reg_penalty = node(x[sample_idx])
+                    loss = loss + reg_penalty * self.regularisation_penalty
+                    pathlogp = pathlogp + logp
                     if len(self.children_dict[str(id(node))]) > 0:
-                        logp, reg_penalty = node.forward(x[idx])
-                        loss += reg_penalty * self.regularisation_penalty
                         for child_node in self.children_dict[str(id(node))]:
-                            nextlevel.append((child_node, pathlogp + logp))
+                            next_level.append((child_node, pathlogp))
                     else:
-                        leaf_probs.append(torch.sum(torch.exp(pathlogp)) * 1.0 / x[idx].shape[0])
-                thislevel = nextlevel
+                        leaf_probs[sample_idx, leaf_idx] = pathlogp.exp().sum(dim=0) / x[sample_idx].shape[0]
+                this_level = next_level
 
-            output['leaf_probs'].append(leaf_probs)
-
-        output['leaf_probs'] = torch.tensor(output['leaf_probs'])
+        output['leaf_probs'] = leaf_probs
         output['y_pred'] = torch.sigmoid(self.linear(output['leaf_probs'])).squeeze(1)
-
         output['reg_loss'] = loss
 
         if len(y) == 0:
             loss = None
         else:
-            loss += self.criterion(output['y_pred'], y)
+            loss = loss + self.criterion(self.linear(output['leaf_probs']).squeeze(1), y)
 
         output['loss'] = loss
         return output