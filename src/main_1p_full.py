bugfix for memory overflow with gpu, and simpler model plotting code added (no previous code deleted for plotting yet)

src/main_1p_full.py
@@ -49,7 +49,7 @@ default_hparams = {
 }
 
 def run_single_panel(hparams, random_state_start=0, model_checkpoint=True):
-
+    torch.cuda.set_device(0)
     if not os.path.exists('../output/%s' % hparams['experiment_name']):
         os.makedirs('../output/%s' % hparams['experiment_name'])
     with open('../output/%s/hparams.csv' % hparams['experiment_name'], 'w') as outfile:
@@ -85,6 +85,10 @@ def run_single_panel(hparams, random_state_start=0, model_checkpoint=True):
                               gate_size_default=hparams['gate_size_default'])
 
         # dafi_tree = run_train_dafi(dafi_tree, hparams, cll_1p_full_input)
+
+        if torch.cuda.is_available():
+            model_tree.cuda()
+            dafi_tree.cuda()
 
         if hparams['two_phase_training']['turn_on']: 
             model_tree, train_tracker, eval_tracker, run_time, model_checkpoint_dict = \

src/train.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from copy import deepcopy
 from random import shuffle
@@ -134,13 +135,22 @@ def init_model_trackers_and_optimizers(hparams, input, model_checkpoint):
         #model_checkpoint_dict.cuda()
     return model, train_tracker, eval_tracker, optimizer_classifier, optimizer_gates, model_checkpoint_dict
 
+
+def free_memory(variables_to_free):
+    for var in variables_to_free:
+        del var
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+
 def run_train_model_two_phase(hparams, input, model_checkpoint=False):
     best_model_so_far = None
     best_log_loss_so_far = 1e10 #just a large number
     for random_init in range(hparams['two_phase_training']['num_random_inits_for_log_loss_only']):
         start = time.time()
         if random_init > 0:
-            #input._get_init_nested_list_(hparams)
+            # have to free memory to avoid overflowing gpu with uneeded copies of the data and trackers
+            #free_memory([model, train_tracker, eval_tracker, optimizer_classifier, 
+            #    optimizer_gates, model_checkpoint_dict, input, output, output_detached])
             if type(input) == Cll8d1pInput:
                 input = Cll8d1pInput(hparams)
             elif type(input) == Cll4d1pInput:
@@ -156,7 +166,6 @@ def run_train_model_two_phase(hparams, input, model_checkpoint=False):
             shuffle(idx_shuffle)
             x_train = [input.x_train[_] for _ in idx_shuffle]
             y_train = input.y_train[idx_shuffle]
-        
             
             if hparams['annealing']['anneal_logistic_k']:
                 #reach the final k by the last epoch starting at init_k
@@ -247,10 +256,11 @@ def run_train_model_two_phase(hparams, input, model_checkpoint=False):
     # Now train using the regularization terms as well as the log loss
     
     start = time.time()
-
+    
     #only train the second part with the best model from before
     model = best_model_so_far
     for epoch in range(hparams['n_epoch'] - hparams['two_phase_training']['num_only_log_loss_epochs']):
+        #free_memory([output, output_detached, x_train, y_train, x_batch, y_batch])
         # shuffle training data
         idx_shuffle = np.array([i for i in range(len(input.x_train))])
         shuffle(idx_shuffle)

src/utils/bayes_gate.py
@@ -31,6 +31,8 @@ class ReferenceTree(object):
     def __init__(self, nested_list, features2id):
         if len(nested_list) != 2:
             raise ValueError("Input 'nested_list' is not properly defined.")
+        self.features2id = features2id
+        self.ids2features = dict((idx, feature) for feature, idx in self.features2id.items())
         self.gate = Gate(nested_list[0], features2id)
         self.n_children = len(nested_list[1])
         self.children = [None] * self.n_children
@@ -112,6 +114,7 @@ class ModelNode(nn.Module):
         gate_low2 = F.sigmoid(self.gate_low2_param)
         gate_upp1 = F.sigmoid(self.gate_upp1_param)
         gate_upp2 = F.sigmoid(self.gate_upp2_param)
+
        
         logp = F.logsigmoid(self.logistic_k * ((x[:, self.gate_dim1] - gate_low1))) \
                + F.logsigmoid(- self.logistic_k * ((x[:, self.gate_dim1] - gate_upp1))) \
@@ -275,7 +278,6 @@ class ModelTree(nn.Module):
                                              output['leaf_probs'][sample_idx][0] / sum(y)
 
         output['loss'] = loss + output['emp_reg_loss']
-
         return output
 
 

src/utils/input.py
@@ -59,10 +59,11 @@ class Cll4d1pInput(CLLInputBase):
         self.x = [torch.tensor(_, dtype=torch.float32) for _ in self.x_list]
         self.y = torch.tensor(self.y_list, dtype=torch.float32)
         on_cuda_list_x_all = []
-        for i in range(len(self.x)):
-            on_cuda_list_x_all.append(self.x[i].cuda())
-        self.x = on_cuda_list_x_all
-        self.y = self.y.cuda()
+        if torch.cuda.is_available():
+            for i in range(len(self.x)):
+                on_cuda_list_x_all.append(self.x[i].cuda())
+            self.x = on_cuda_list_x_all
+            self.y = self.y.cuda()
 
     def _load_data_(self, hparams):
         #DATA_DIR = '../data/cll/'
@@ -250,6 +251,13 @@ class Cll8d1pInput(Cll4d1pInput):
 
         self.x = [torch.tensor(_, dtype=torch.float32) for _ in self.x_list]
         self.y = torch.tensor(self.y_list, dtype=torch.float32)
+
+        on_cuda_list_x_all = []
+        if torch.cuda.is_available():
+            for i in range(len(self.x)):
+                on_cuda_list_x_all.append(self.x[i].cuda())
+            self.x = on_cuda_list_x_all
+            self.y = self.y.cuda()
 
     def _load_data_(self, hparams):
         X_DATA_PATH = hparams['data']['features_path']